cf0e35449c05352f5d3a21aa548a05a05a629992
mamon6060 <mamon.okobiz@gmail.com>
2025-05-06 13:23:01 +0600

    working landing page

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,10 +1,14 @@
 import Banner from "@/components/home/Banner/Banner";
 import RentSection from "@/components/home/RentSection/RentSection";
 import { getAllBanners } from "@/services/banners";
+import { getAllRents } from "@/services/rents";
 import React from "react";
 
 const page = async () => {
   const { data: banners } = await getAllBanners();
+  const { data: rents } = await getAllRents();
+
+  console.log("try to find all rents",rents)
   return (
     <div>
       <Banner banners={banners} />
--- src/components/home/RentSection/RentSection.tsx
+++ src/components/home/RentSection/RentSection.tsx
@@ -1,8 +1,19 @@
+"use client";
 import RentCard from "@/components/card/RentCard/RentCard";
 import SectionTitle from "@/utilits/SectionTitle";
-import React from "react";
+import { useEffect, useState } from "react";
 
 const RentSection = () => {
+  const [rents, setRents] = useState([]);
+  useEffect(() => {
+    fetch("rentData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setRents(data);
+      });
+  }, []);
+
+  console.log("find rent data", rents);
   return (
     <div className="Container py-12">
       <div>
@@ -13,10 +24,9 @@ const RentSection = () => {
       </div>
 
       <div className="mt-8 grid grid-cols-4 gap-4">
-        <RentCard />
-        <RentCard />
-        <RentCard />
-        <RentCard />
+        {rents?.map((rent) => (
+          <RentCard key={rent._id} rent={rent}></RentCard>
+        ))}
       </div>
     </div>
   );
