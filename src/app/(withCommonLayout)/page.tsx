d8f1ae097258c8c1f13dc1e5709929bba1cd6bbd
mamon6060 <mamon.okobiz@gmail.com>
2025-05-08 13:57:00 +0600

    priviews code

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,4 +1,6 @@
 import Banner from "@/components/home/Banner/Banner";
+import FlatsSection from "@/components/home/FlatsSection/FlatsSection";
+
 import RentSection from "@/components/home/RentSection/RentSection";
 import { getAllBanners } from "@/services/banners";
 import { getAllRents } from "@/services/rents";
@@ -8,11 +10,12 @@ const page = async () => {
   const { data: banners } = await getAllBanners();
   const { data: rents } = await getAllRents();
 
-  console.log("try to find all rents",rents)
+  console.log("try to find all rents", rents);
   return (
     <div>
       <Banner banners={banners} />
       <RentSection />
+      <FlatsSection />
     </div>
   );
 };
