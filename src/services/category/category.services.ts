b7d7437bad67a44a406fb8f8f0696831db2c52ab
mamon6060 <mamon.okobiz@gmail.com>
2025-05-24 10:52:21 +0600

    almost 10 times fixed the same type error relative host layout

--- src/services/category/category.services.ts
+++ src/services/category/category.services.ts
@@ -1,4 +1,4 @@
-import { CategoryResponse } from "@/app/(HostLayout)/components/types/category";
+import { CategoryResponse } from "@/app/(hostLayout)/components/types/category";
 import CategoryApis from "@/app/apis/category.apis";
 
 const {
