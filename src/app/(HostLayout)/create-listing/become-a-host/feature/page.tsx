878662008348cdb194d62d6ee049e61ca3310c14
shohid-okobiz <dev.okobiz@gmail.com>
2025-05-26 17:07:02 +0600

    changed

--- src/app/(HostLayout)/create-listing/become-a-host/feature/page.tsx
+++ src/app/(HostLayout)/create-listing/become-a-host/feature/page.tsx
@@ -9,8 +9,6 @@ import { Feature } from "@/types/blogTypes/blogTypes";
 import { FeatureType } from "@/app/(HostLayout)/components/types/feature";
 import { ListingResponse } from "@/app/(HostLayout)/components/types/listing";
 import { Skeleton } from "antd";
-import { FeatureType } from "@/app/(hostLayout)/components/types/feature";
-import { ListingResponse } from "@/app/(hostLayout)/components/types/listing";
 
 export default function FeaturePage() {
   const [features, setFeatures] = useState<Feature[]>([]);
